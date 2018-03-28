7a9deced9614dd4778fc2bdfbf2a3a07d2e345be
Cameron Fletcher <codenameflip@gmail.com>
2018-03-28 02:01:21 +0000

    untest

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ var EXERCISES = [
         ],
         form: [
             { id: 1, question: "How much garbage (total) was received on Day 1?", answer: 7940 },
-            { id: 2, question: "How much garbage (total) was received on Day 6?", answer: 6931 }, //test from cloud9
+            { id: 2, question: "How much garbage (total) was received on Day 6?", answer: 6931 },
             { id: 3, question: "Which type of garbage was most received on Day 4?", answer: "trash" }
         ],
         finish_code: 19701
