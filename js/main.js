74f2f7720869dba4fc911b347bad2952089c688f
Cameron Fletcher <codenameflip@gmail.com>
2018-03-28 13:27:35 +0000

    added exe2

--- js/main.js
+++ js/main.js
@@ -29,6 +29,22 @@ var EXERCISES = [
             { id: 3, question: "Which type of garbage was most received on Day 4?", answer: "trash" }
         ],
         finish_code: 19701
+    },
+    {
+        id: 2,
+        title: "Wordsmith",
+        description: "",
+        code: "https://gist.github.com/camfletcher/95cf5fb25273d18775f0051d3d669123",
+        skeleton: "https://google.com",
+        steps: [
+            "Print a string containing all the inputted words, sorted alphanumerically",
+            "Print a string containing the number of words in your output",
+            "Print the number of words each character in the alphabet has in your output"
+        ],
+        form: [
+            { id: 1, question: "What are the first 10 words that start with 'b'? (ex: \"wordOne wordTwo wordThree ...\")", answer: "baby back background bacon bad bag bait bake balance bald" },
+            { id: 2, question: "How many words are there total in the output?", answer: 10 }
+        ]
     }
 ]
 
