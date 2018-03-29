fc47515adb0d9f50e4af27db15f50ef4667b75cb
Cameron Fletcher <codenameflip@gmail.com>
2018-03-29 12:37:43 +0000

    Reordered challenges

--- js/config.js
+++ js/config.js
@@ -1,24 +1,6 @@
 const EXERCISES = [
     {
         id: 1,
-        title: "The First Exercise",
-        description: "You work at a waste management facility and your employer needs you to write a program that can be used to provide statistics about the daily intake of waste the facility receives.<br>There are 3 types of garbage: \"t\" (trash), \"r\" (recycleable), \"f\" (food).<br>Your program will be supplied an input that looks similar to \"rrftftff\" and you will have to use that input to complete the tasks mentioned below. You will have a different string of characters to represent 7 days worth of garbage collection. (If you're confused, refer to the input provided.)",
-        code: "https://gist.github.com/camfletcher/eef8f1d6c93e0c949db8c1c55d845cb4",
-        skeleton: "https://gist.github.com/camfletcher/3ca415e62e4f0bac1a9f41e892f934e3",
-        steps: [
-            "Print the number of each type of garbage that was received per day",
-            "Print the most common type of trash received per day",
-            "Print the sum of all the items collected for each day"
-        ],
-        form: [
-            { id: 1, question: "How much garbage (total) was received on Day 1?", answer: 7940 },
-            { id: 2, question: "How much garbage (total) was received on Day 6?", answer: 6931 },
-            { id: 3, question: "Which type of garbage was most received on Day 4?", answer: "trash" }
-        ],
-        finish_code: "19701"
-    },
-    {
-        id: 2,
         title: "Wordsmith",
         description: "You have been given a list of words that need to be sorted before they're added to the dictionary! Write a program that will sort each word alphanumerically and outputs a string containing the sorted words.",
         code: "https://gist.github.com/camfletcher/95cf5fb25273d18775f0051d3d669123",
@@ -40,6 +22,24 @@ const EXERCISES = [
             { id: 5, question: "What word comes after 'dressing'?", answer: "dribble" }
         ],
         finish_code: "19703"
+    },
+    {
+        id: 2,
+        title: "Waste Management",
+        description: "You work at a waste management facility and your employer needs you to write a program that can be used to provide statistics about the daily intake of waste the facility receives.<br>There are 3 types of garbage: \"t\" (trash), \"r\" (recycleable), \"f\" (food).<br>Your program will be supplied an input that looks similar to \"rrftftff\" and you will have to use that input to complete the tasks mentioned below. You will have a different string of characters to represent 7 days worth of garbage collection. (If you're confused, refer to the input provided.)",
+        code: "https://gist.github.com/camfletcher/eef8f1d6c93e0c949db8c1c55d845cb4",
+        skeleton: "https://gist.github.com/camfletcher/3ca415e62e4f0bac1a9f41e892f934e3",
+        steps: [
+            "Print the number of each type of garbage that was received per day",
+            "Print the most common type of trash received per day",
+            "Print the sum of all the items collected for each day"
+        ],
+        form: [
+            { id: 1, question: "How much garbage (total) was received on Day 1?", answer: 7940 },
+            { id: 2, question: "How much garbage (total) was received on Day 6?", answer: 6931 },
+            { id: 3, question: "Which type of garbage was most received on Day 4?", answer: "trash" }
+        ],
+        finish_code: "19701"
     },
     {
         id: 3, 
