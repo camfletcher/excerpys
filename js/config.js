ae9021f568ab88e0c4d02f4bd0aaeb50349c0aa2
codenameflip <codenameflip@gmail.com>
2018-03-28 20:45:01 -0400

    Added new challenge

--- js/config.js
+++ js/config.js
@@ -36,6 +36,23 @@ const EXERCISES = [
             { id: 5, question: "What word comes after 'dressing'?", answer: "dribble" }
         ],
         finish_code: "19703"
+    },
+    {
+        id: 3, 
+        title: "Recursive Calculator",
+        description: "NASA Scientists are calculating large strings of data using super computers. Unfortunately the application they were using broke, now they need you to write a solution to their problem.<br><br>Input looks like the following: '52223345555'...<br><br>They need you to help them calculate the hidden decimal number within that string.",
+        code: "https://gist.github.com/camfletcher/5d7e9fcf45b8f500c9757a4d7bc24b57",
+        skeleton: "https://gist.github.com/camfletcher/3598be991043ddf28e58c4b45dbb418f",
+        steps: [
+            "If a character has a repeating EVEN number, add the value of the current number to your output",
+            "If a character has a repeating ODD number, subtract that value of the current number from your output",
+            "If a character does NOT have a repeating characer, multiply the current output by .5",
+            "Print the calculated output"
+        ],
+        form: [
+            { id: 1, question: "What is the calculated number?", answer: -0.883302189894 }
+        ],
+        finish_code: "19705"
     }
 ];
 
